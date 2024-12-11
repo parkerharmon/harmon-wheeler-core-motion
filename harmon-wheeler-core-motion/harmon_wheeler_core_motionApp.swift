af0bc9b26d9963cbae3adad080e6f75ccf07a2ca
Parker <parkerharmon194@gmail.com>
2024-12-11 03:22:44 -0500

    Initial Commit

---
+++ harmon-wheeler-core-motion/harmon_wheeler_core_motionApp.swift
@@ -0,0 +1,17 @@
+//
+//  harmon_wheeler_core_motionApp.swift
+//  harmon-wheeler-core-motion
+//
+//  Created by Alissa Pavano on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct harmon_wheeler_core_motionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ harmon-wheeler-core-motionTests/harmon_wheeler_core_motionTests.swift
@@ -0,0 +1,17 @@
+//
+//  harmon_wheeler_core_motionTests.swift
+//  harmon-wheeler-core-motionTests
+//
+//  Created by Alissa Pavano on 12/11/24.
+//
+
+import Testing
+@testable import harmon_wheeler_core_motion
+
+struct harmon_wheeler_core_motionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
